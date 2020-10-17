98fbeabad9594aab0c58bec45f2b24a2b4945e90
alex mextner <Aleksandr.MEKHONOSHIN@raiffeisen.ru>
2020-10-17 20:57:34 +0300

    Updates

---
+++ precache-manifest.7f5f31337e6e2eebb9a41e8c57924e47.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f759273f032317aaf91b358487c6b6b",
+    "url": "/telegram-react/index.html"
+  },
+  {
+    "revision": "4cb57df6c961ad965382",
+    "url": "/telegram-react/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "0985785fe38fe6b153f5",
+    "url": "/telegram-react/static/css/3.5698f36c.chunk.css"
+  },
+  {
+    "revision": "31beb6398631ea0d2233",
+    "url": "/telegram-react/static/css/5.6ede3073.chunk.css"
+  },
+  {
+    "revision": "7f7b9b86fd4337572fa2",
+    "url": "/telegram-react/static/css/7.3b3f13e5.chunk.css"
+  },
+  {
+    "revision": "7e507d2725f655f9faa3",
+    "url": "/telegram-react/static/css/main.02d1675c.chunk.css"
+  },
+  {
+    "revision": "4cb57df6c961ad965382",
+    "url": "/telegram-react/static/js/0.2b3498d8.chunk.js"
+  },
+  {
+    "revision": "0985785fe38fe6b153f5",
+    "url": "/telegram-react/static/js/3.7ee9e03a.chunk.js"
+  },
+  {
+    "revision": "5d1d6064e1de6968ba1f",
+    "url": "/telegram-react/static/js/4.e9bce4ec.chunk.js"
+  },
+  {
+    "revision": "31beb6398631ea0d2233",
+    "url": "/telegram-react/static/js/5.86967459.chunk.js"
+  },
+  {
+    "revision": "f9d470b36c81d912d989",
+    "url": "/telegram-react/static/js/6.8a08043e.chunk.js"
+  },
+  {
+    "revision": "7f7b9b86fd4337572fa2",
+    "url": "/telegram-react/static/js/7.68ac436b.chunk.js"
+  },
+  {
+    "revision": "7e507d2725f655f9faa3",
+    "url": "/telegram-react/static/js/main.6ad262f4.chunk.js"
+  },
+  {
+    "revision": "bc644eb7c6c3908feed1",
+    "url": "/telegram-react/static/js/runtime~main.f88c51b2.js"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/telegram-react/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/telegram-react/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telegram-react/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-react/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
