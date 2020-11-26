65f6b6f691274b37111d0e36c73edd4fd0f2bee8
alex mextner <Aleksandr.MEKHONOSHIN@raiffeisen.ru>
2020-11-26 21:28:24 +0300

    Updates

---
+++ precache-manifest.cf85a7eb3e5b0ca9e7d19b9680a2293a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea47c413587e4f1f5c5a54d4be77d54",
+    "url": "/telegram-asana/index.html"
+  },
+  {
+    "revision": "f6e8bbdea58bab27667a",
+    "url": "/telegram-asana/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "239673e160a9b7bea928",
+    "url": "/telegram-asana/static/css/4.0640dbf3.chunk.css"
+  },
+  {
+    "revision": "10c893eb4ad526bef09f",
+    "url": "/telegram-asana/static/css/5.6ede3073.chunk.css"
+  },
+  {
+    "revision": "f9140cdfc628d11c61d7",
+    "url": "/telegram-asana/static/css/7.3b3f13e5.chunk.css"
+  },
+  {
+    "revision": "60b64a857aa579ac7e2b",
+    "url": "/telegram-asana/static/css/main.d0ab572a.chunk.css"
+  },
+  {
+    "revision": "f6e8bbdea58bab27667a",
+    "url": "/telegram-asana/static/js/0.f4827c62.chunk.js"
+  },
+  {
+    "revision": "37f0ee77d420ec42902a",
+    "url": "/telegram-asana/static/js/3.faad88c4.chunk.js"
+  },
+  {
+    "revision": "239673e160a9b7bea928",
+    "url": "/telegram-asana/static/js/4.cfbc619a.chunk.js"
+  },
+  {
+    "revision": "10c893eb4ad526bef09f",
+    "url": "/telegram-asana/static/js/5.40a380eb.chunk.js"
+  },
+  {
+    "revision": "844a259837db7c37a42d",
+    "url": "/telegram-asana/static/js/6.7911925f.chunk.js"
+  },
+  {
+    "revision": "f9140cdfc628d11c61d7",
+    "url": "/telegram-asana/static/js/7.1b7b3268.chunk.js"
+  },
+  {
+    "revision": "60b64a857aa579ac7e2b",
+    "url": "/telegram-asana/static/js/main.5b32c9c0.chunk.js"
+  },
+  {
+    "revision": "687ee32a22c8670cbd11",
+    "url": "/telegram-asana/static/js/runtime~main.90e78d2f.js"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/telegram-asana/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/telegram-asana/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telegram-asana/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-asana/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-asana/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
